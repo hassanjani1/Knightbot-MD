46eaef7dd24823c7a4bf109b34df48bcf94da01b
hassanjani1 <161316228+hassanjani1@users.noreply.github.com>
2025-09-17 03:56:45 +0500

    Update autostatus.js

--- commands/autostatus.js
+++ commands/autostatus.js
@@ -6,8 +6,8 @@ const channelInfo = {
         forwardingScore: 1,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363161513685998@newsletter',
-            newsletterName: 'KnightBot MD',
+            newsletterJid: '120363419893616380@newsletter',
+            newsletterName: 'Rana-Awais MD',
             serverMessageId: -1
         }
     }
